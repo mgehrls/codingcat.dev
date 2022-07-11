b9d8fce6ff2f7eb333542761170f58726c8d3b23
Alex Patterson <alex@codingcat.dev>
2022-07-11 13:52:21 -0400

    bad type

--- frontend/overlay/models/user.model.ts
+++ frontend/overlay/models/user.model.ts
@@ -1,52 +1,51 @@
-import { NotionBlock } from '@9gustin/react-notion-render';
 import { UserInfo } from 'firebase/auth';
 import { Cloudinary } from './cloudinary.model';
 import { SocialLink } from './site.model';
 
 export interface UserInfoExtended extends UserInfo {
-  memberships?: UserMembership[];
-  token?: string;
-  basicInfo?: BasicUserInfo;
-  roles?: Roles[];
+	memberships?: UserMembership[];
+	token?: string;
+	basicInfo?: BasicUserInfo;
+	roles?: Roles[];
 }
 
 export interface Author {
-  _id: string;
-  id: string;
-  blocks?: NotionBlock[];
-  basicInfo?: BasicUserInfo;
-  displayName: string;
-  photoURL?: Cloudinary;
-  slug: string;
+	_id: string;
+	id: string;
+	blocks?: any[];
+	basicInfo?: BasicUserInfo;
+	displayName: string;
+	photoURL?: Cloudinary;
+	slug: string;
 }
 
 export interface UserMembership {
-  membership: boolean;
-  membershipType: MembershipType;
-  subscriptionId: string;
+	membership: boolean;
+	membershipType: MembershipType;
+	subscriptionId: string;
 }
 
 export enum MembershipType {
-  admin = 'admin',
-  editor = 'editor',
-  author = 'author',
+	admin = 'admin',
+	editor = 'editor',
+	author = 'author'
 }
 
 export enum Roles {
-  supporter = 'supporter',
-  monthly = 'monthly',
-  yearly = 'yearly',
+	supporter = 'supporter',
+	monthly = 'monthly',
+	yearly = 'yearly'
 }
 
 export interface BasicUserInfo {
-  location?: string;
-  about?: string;
-  website?: string;
+	location?: string;
+	about?: string;
+	website?: string;
 
-  socialLinks?: SocialLink[];
+	socialLinks?: SocialLink[];
 }
 
 export enum AuthIssue {
-  mustsignin = 'mustsignin',
-  unauthorized = 'unauthorized',
+	mustsignin = 'mustsignin',
+	unauthorized = 'unauthorized'
 }
